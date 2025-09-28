c70671456cad8a3f081a875c7ea522846b8415bc
Gabriele <gpolastri117@gmail.com>
2025-09-28 09:06:03 +0200

    Add files via upload

---
+++ Problems 709-To Lower Case.py
@@ -0,0 +1,11 @@
+#Problems 709. To Lower Case
+"""
+Given a string s, return the string after replacing every uppercase letter
+with the same lowercase letter.
+"""
+
+class Solution:
+    def toLowerCase(self, s: str) -> str:
+        s = s.lower()
+        return s
+
