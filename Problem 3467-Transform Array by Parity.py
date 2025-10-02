b3c807c1b7a146d50149ab8760f298a6384535a3
Gabriele <gpolastri117@gmail.com>
2025-10-02 08:57:49 +0200

    Add files via upload

---
+++ Problem 3467-Transform Array by Parity.py
@@ -0,0 +1,13 @@
+#Problem 3467-Transform Array by Parity
+
+
+
+nums=[4,3,2,1]
+
+for i in range(len(nums)):
+    if nums[i]%2==0:
+        nums[i]=0
+    else:
+        nums[i]=1
+nums.sort()
+print(nums)
